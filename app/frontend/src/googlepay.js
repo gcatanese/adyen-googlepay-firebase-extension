aa12c908e7c92f4fe47f94eea884e5d4be2cd309
Beppe Catanese <gcatanese@yahoo.com>
2022-05-17 13:48:29 +0200

    WIP Add order with GooglePay button

--- app/frontend/src/googlepay.js
+++ app/frontend/src/googlepay.js
@@ -5,41 +5,91 @@ import { withRouter } from 'react-router-dom';
 import GooglePayButton from '@google-pay/button-react';
 
 import {db} from './Firestore'
-import {collection, addDoc, Timestamp} from 'firebase/firestore'
+import {collection, addDoc, getDoc, onSnapshot, query, where} from 'firebase/firestore'
 
 import Header from './header'
 import Container from '@mui/material/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 class CheckoutGooglePay extends React.Component {
 
     constructor() {
         super();
+
+        this.state = {
+            'order': {}
+        }
     }
 
     async componentDidMount() {
-
         const orderId =  this.props.match.params.order;
         const resp = await axios.get('/api/order/' + orderId);
-        const order = resp.data;
+
+        this.setState({'order': resp.data})
     }
 
     async addPaymentRequest(amount, currency, token) {
-
         const ref = await addDoc(collection(db, 'payments'), {
             psp: 'adyen',
-            total: 100,
+            total: amount,
             currency: currency,
             paymentToken: token
         })
+        console.log(ref);
+        console.log('doc id: ' + ref.id);
+
+//        const observer = onSnapshot(ref, docSnapshot => {
+//            console.log(docSnapshot);
+//        }, err => {
+//            console.log(`Encountered error: ${err}`);
+//        });
+
+        //const q = query(collection(db, "payments"), where("psp", "==", 'adyen'));
+        const q = query(collection(db, "payments"), where("__name__", "==", ref.id));
+
+        const observer = onSnapshot(q, docSnapshot => {
+            docSnapshot.docChanges().forEach(change => {
+                if (change.type === "added") {
+                    console.log("record created -> ", change.doc.data());
+                }
+                if (change.type === "modified") {
+                    console.log("record updated ", change.doc.data());
+                }
+            });
+        }, err => {
+            console.log(`Encountered error: ${err}`);
+        });
+
     }
 
-
     render() {
+
+       const {order} = this.state;
 
         return (
             <div>
                 <Header/>
                 <Container maxWidth="md">
+                    { /* product container */ }
+                    <Card  raised="true" >
+                        <br/>
+                        <div style= {{width: "75%", margin: "auto", textAlign: "center", paddingTop: "2px"}} >
+                            <img
+                                alt = "Your tree"
+                                style = {{ width: "50%", height: "50%"}}
+                                src = {order.image}
+                            />
+                            <Typography variant="body1">{order.title}</Typography>
+                            <Box fontWeight="fontWeightLight">(pic by {order.author})</Box>
+                        </div>
+                        <br/>
+                    </Card>
+                    <br/>
+                    { /* gpay container */ }
+                    <Grid container justify = "center">
                     <GooglePayButton
                         environment="TEST"
                         paymentRequest={{
@@ -68,19 +118,20 @@ class CheckoutGooglePay extends React.Component {
                             transactionInfo: {
                               totalPriceStatus: 'FINAL',
                               totalPriceLabel: 'Total',
-                              totalPrice: '100.00',
-                              currencyCode: 'USD',
-                              countryCode: 'US',
+                              totalPrice: '13.00',
+                                currencyCode: 'USD',
+                                countryCode: 'US',
                             },
                         }}
-                    onLoadPaymentData={paymentRequest => {
-                        console.log('load payment data', paymentRequest);
-                        var token = paymentRequest.paymentMethodData.tokenizationData.token;
-
-                        this.addPaymentRequest(120, 'EUR', token);
+                        onLoadPaymentData={ paymentRequest => {
+                            console.log('load payment data', paymentRequest);
+                            var token = paymentRequest.paymentMethodData.tokenizationData.token;
+                            /* add payment to Firecloud */
+                            this.addPaymentRequest(order.amount, 'EUR', token);
+                            }
                         }
-                    }
-                />
+                    />
+                </Grid>
                 </Container>
             </div>
         );
